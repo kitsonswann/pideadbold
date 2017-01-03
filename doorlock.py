40190a33ea126464429c81bf59ce774d7f812270
Kitson Swann <kptswann@gmail.com>
2017-01-03 09:52:05 -0800

    Remove creds

--- doorlock.py
+++ doorlock.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-"""account_sid = "AC494595b37f8a8a903d4fe4bb3f9a46b0"
-auth_token  = "a0e519a5d6bea59418b265f5abb82b2f"
+"""account_sid = "#############"
+auth_token  = "############"
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="Hello Kitson - This is Pi",
